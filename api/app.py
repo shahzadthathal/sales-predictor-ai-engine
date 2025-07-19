9804e6f44918d0c3d45795586b1df273bdd3c310
Shahzad <shahzad@esm.ae>
2025-07-19 16:08:57 +0400

    binding to 0.0.0.0 and the correct port for Replit.

--- api/app.py
+++ api/app.py
@@ -2,12 +2,14 @@
 
 from flask import Flask
 from api.routes import v1
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(v1)
+    app.register_blueprint(v1, url_prefix="/api/v1")
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
